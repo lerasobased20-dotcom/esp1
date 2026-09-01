f7c70cf5d8f3372e41429dc2a1e47308117cc557
lerasobased <lerasobased20@gmail.com>
2026-09-01 23:33:20 +0500

    Delete jni directory

--- jni/Android.mk
+++
@@ -1,9 +0,0 @@
-LOCAL_PATH := $(call my-dir)
-include $(CLEAR_VARS)
-LOCAL_MODULE := esp
-LOCAL_SRC_FILES := main.cpp
-LOCAL_C_INCLUDES := $(LOCAL_PATH)
-LOCAL_CPPFLAGS := -std=c++17 -fno-rtti -fno-exceptions -Oz -fvisibility=hidden
-LOCAL_LDFLAGS := -Wl,--gc-sections -Wl,--strip-all -pie
-LOCAL_LDLIBS := -llog -landroid -lEGL -lGLESv3
-include $(BUILD_EXECUTABLE)
--- jni/Application.mk
+++
@@ -1,3 +0,0 @@
-APP_ABI := arm64-v8a
-APP_PLATFORM := android-23
-APP_STL := c++_static
